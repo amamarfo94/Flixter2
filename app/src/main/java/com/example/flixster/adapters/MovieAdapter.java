fe975bedbf60079bae8f5930de212fd41522f551
amamarfo94 <marfoama43@gmail.com>
2020-02-13 19:34:34 -0500

    comment

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,18 +1,25 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.model.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -49,8 +56,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         return _movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder<container> extends RecyclerView.ViewHolder {
 
+       RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -60,12 +68,26 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(_context).load(movie.getPosterPath()).into(ivPoster);
-        }
+            //1. Register click lister on whole container
+            //2. Navigate to a new activity on tap
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                 Intent i = new Intent(_context, DetailActivity.class);
+                 i.putExtra("movie", Parcels.wrap(movie));
+
+
+                 _context.startActivity(i);
+
+                }
+        });
     }
-}
+}}
--- app/src/main/java/com/example/flixster/model/Movie.java
+++ app/src/main/java/com/example/flixster/model/Movie.java
@@ -3,23 +3,29 @@ package com.example.flixster.model;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+@Parcel
 
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
+    double rating;
 
     String BASE_URL = "https://image.tmdb.org/t/p/";
     String POSTER_W3532 = "w342/";
+    // empty constructor needed by the Parceler library
+     public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average:");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -41,5 +47,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
